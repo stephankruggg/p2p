28171c857af2ea0aae3396e627b1e896edaed8a7
Stephan Krug <stekrug@icloud.com>
2024-10-14 23:32:58 -0300

    feat: Add UDP server

---
+++ src/models/udpserver.py
@@ -0,0 +1,57 @@
+import threading
+import socket
+import struct
+from time import sleep
+
+from utils.constants import Constants
+
+class UDPServer(threading.Thread):
+    def __init__(self, address, port, peer):
+        super().__init__()
+
+        self._peer = peer
+        self._address = address
+        self._port = port
+
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.bind((self._address, self._port))
+
+    @property
+    def address(self):
+        return self._address
+
+    @property
+    def address(self):
+        return (self._address, self._port)
+
+    def run(self):
+        while True:
+            print(f'UDP Server (Address: {self._address}, Port: {self._port}) listening...')
+
+            data = self._socket.recv(1024)
+
+            ttl, requester_id, requester_address, requester_port, requested_file = struct.unpack(Constants.FLOODING_REQUEST_FORMAT, data)
+            requested_file = requested_file.rstrip(b'\x00').decode('utf-8')
+            requester_address = socket.inet_ntoa(requester_address)
+            print(f'Received request from ID -> {requester_id}, ADDRESS -> {requester_address}, PORT -> {requester_port}: TTL -> {ttl}, FILENAME -> {requested_file}')
+
+            peer_folder = Constants.FILES_PATH / str(self._peer.id)
+            filenames = [f.name for f in peer_folder.iterdir() if f.is_file()]
+            chunks = []
+            entire_file = False
+
+            for f in filenames:
+                if f.startswith(requested_file):
+                    if f == requested_file:
+                        entire_file = True
+                        continue
+
+                    chunks.append(f)
+
+            response = self._peer.flooding_response(chunks, entire_file)
+            self._socket.sendto(response, (requester_address, requester_port))
+
+            ttl -= 1
+            if ttl > 0:
+                sleep(1)
+                self._peer.reroute(ttl, requester_id, requester_address, requester_port, requested_file)
